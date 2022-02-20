f65dde97aceff66f2d82690320f84b2d6064ff14
Diego <diego_ melo_1991@hotmail.com>
2022-02-20 14:52:14 -0300

    ProductService

--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -64,7 +64,7 @@ public class ProductService {
 	public Page<ProductDTO> findAllPaged(Long categoryId, String name, Pageable pageable) {
 		List<Category> category = (categoryId == 0) ? null : Arrays.asList(categoryRepository.getOne(categoryId));
 		Page<Product> page = repository.find(category, name, pageable);
-		//repository.findProductsWithCategories(page.getContent());
+		repository.findProductsWithCategories(page.getContent());
 		return page.map(x -> new ProductDTO(x, x.getCategories()));
 	}
 	
